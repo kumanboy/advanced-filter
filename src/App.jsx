96f27a36ebe373aa29d38942d01436a6c235418b
kumanboy <husandavronov13@gmail.com>
2025-02-17 10:07:45 +0500

    products n navbar end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./Navigation/Nav.jsx";
 import Products from "./Products/Products.jsx";
 import Recommended from "./Recommended/Recommended.jsx";
+import "./index.css"
 
 const App = () => {
     return (
