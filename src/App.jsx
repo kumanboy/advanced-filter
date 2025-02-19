76029085e433bbd5197eb2f66cc7f6a0985333c1
kumanboy <husandavronov13@gmail.com>
2025-02-19 11:20:43 +0500

    sidebar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "./Navigation/Nav.jsx";
 import Products from "./Products/Products.jsx";
 import Recommended from "./Recommended/Recommended.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
 import "./index.css"
+
 
 const App = () => {
     return (
         <>
+            <Sidebar/>
             <Nav/>
             <Recommended/>
             <Products />
